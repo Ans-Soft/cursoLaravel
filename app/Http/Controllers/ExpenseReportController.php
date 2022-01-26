7c98194dd168dbcfb22058f82225b57c1f0a0b96
isabellasantafe <isabella.santafe@mariaelvinia.edu.co>
2022-01-26 18:32:06 -0500

    commit 1.0
    Error en el envio de mails.

--- app/Http/Controllers/ExpenseReportController.php
+++ app/Http/Controllers/ExpenseReportController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ExpenseReport;
+use App\Mail\SummaryReport;
+use Illuminate\Support\Facades\Mail;
 
 class ExpenseReportController extends Controller
 {
@@ -64,7 +66,10 @@ class ExpenseReportController extends Controller
      */
     public function show($id)
     {
-        //
+        $report = ExpenseReport::findOrFail($id);
+        return view("expenseReport.show", [
+            "report" => $report 
+        ]);
     }
 
     /**
@@ -122,5 +127,21 @@ class ExpenseReportController extends Controller
         return view("expenseReport.confirmDelete", [
             "report" => $report
         ]);
+    }
+
+    public function confirmSendEmail($id) {
+        $report = ExpenseReport::find($id);
+        return view("expenseReport.confirmSendEmail", [
+            "report" => $report
+        ]);
+    }
+
+    public function sendEmail(Request $request, $id) {
+        $report = ExpenseReport::find($id);
+        Mail::to($request->get("email"))->send(new SummaryReport($report));
+        
+        return $redirect("/expense_reports/" . $id);
+
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExpenseReportController;
+use App\Http\Controllers\ExpenseController;
 
 Route::get("/", [homeController::class, "index"]);
 
@@ -23,3 +24,11 @@ Route::get("/dashboard", [DashboardController::class, "index"]);
 Route::resource("/expense_reports", ExpenseReportController::class);
 
 Route::get("/expense_reports/{id}/confirmDelete", [ExpenseReportController::class, "confirmDelete"]);
+
+Route::get("/expense_reports/{id}/confirmSendEmail", [ExpenseReportController::class, "confirmSendEmail"]);
+
+Route::post("/expense_reports/{id}/sendEmail", [ExpenseReportController::class, "sendEmail"]);
+
+Route::get("/expense_reports/{expense_report}/expenses/create", [ExpenseController::class, "create"]);
+
+Route::post("/expense_reports/{expense_report}/expenses", [ExpenseController::class, "store"]);
